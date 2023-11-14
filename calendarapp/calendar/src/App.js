3f68d5a402acd37a67d676770dac6221c8e588f2
Lee Campbell <121746971+leevcampbell@users.noreply.github.com>
2023-11-14 12:23:14 -0500

    created calendar template, no functionality

---
+++ calendarapp/calendar/src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, { useState } from 'react';
+import DatePicker from "react-datepicker";
+
+
+const locales = {
+  'en-US': require('date-fns/locale/en-US'),
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Drs. Appointment',
+  allDay: true,
+  start: new Date(2023,11,15),
+  end: new Date(2023,11,15),
+},
+{
+  title: 'Conference with Mitty',
+  start: new Date(2023,11,13),
+  end: new Date(2023,11,13),
+},
+{
+  title: 'Waterfall Trip',
+  start: new Date(2023,11,14),
+  end: new Date(2023,11,20)
+}
+]
+
+function App() {
+  const [newEvent, setNewEvent] = useState({title: '', start: '', end: ''})
+  const [allEvents, setAllEvents] = useState(events)
+
+  const handleAddEvent = (e) => {
+    e.preventDefault()
+    setAllEvents([...allEvents, newEvent])
+  }
+
+  return (
+    <div className="App">
+      <h1>Calendar</h1>
+     <Calendar 
+     localizer={localizer} 
+     events={allEvents} 
+     startAccessor="start" 
+     endAccessor="end" style={{ height: 500, margin: "50px" }} />
+    </div>
+  );
+}
+
+export default App;
