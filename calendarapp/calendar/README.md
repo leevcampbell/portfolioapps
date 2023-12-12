79bfb3d99a5e2aa92a45bf2b186ae0f5845c6c2c
Lee Campbell <121746971+leevcampbell@users.noreply.github.com>
2023-12-12 13:51:58 -0500

    added README's for calculator and Calendar

--- calendarapp/calendar/README.md
+++ calendarapp/calendar/README.md
@@ -1,2 +1,60 @@
-Extremely simple Frontend for a calendar app. Built using react-big-calendar with some dummy data. Can create events and see them posted on Calendar, events do not persist on refresh. Strictly frontend, no backend. 
-```
+**React Big Calendar App**
+
+**Overview**
+
+This is a simple calendar app built using React and react-big-calendar. The app provides an intuitive and interactive interface for managing events and appointments. With react-big-calendar, users can easily view and create events on a monthly, weekly, or daily basis. The app has been deployed to Surge and can be viewed at http://simply-calendar.surge.sh
+
+**Features**
+
+1. Monthly, weekly, and daily views
+2. Responsive design for various screen sizes
+3. Easily add events
+4. Instanyly view newly created events on the calendar
+
+**Getting Started**
+To run the calendar app locally on your machine, follow these steps:
+
+1. Clone the repository:
+    ```
+    git clone https://github.com/leevcampbell/portfolioapps.git
+    ```
+
+2. Navigate to the calendar directory:
+    ```
+    cd calendarapp/calendar
+    ```
+
+3. Install the dependencies:
+    ```
+    npm install
+    ```
+
+4. Run the app:
+    ```
+    npm start
+    ```
+
+The app should now be running on http://localhost:3000/
+
+**Usage**
+
+1. Enter event name and select start and end dates
+2. Click on the "Add Event" button to add the event to the calendar
+3. Click on the "Month", "Week", and "Day" buttons to switch between views
+4. Click on the "Today" button to return to the current date
+5. Click Back or Next to view previous or upcoming months
+
+**Technologies Used**
+
+1. React
+2. react-big-calendar
+3. JavaScript
+4. HTML
+5. CSS
+
+**Future Improvements**
+
+1. Add backend to persist events
+2. Add ability to edit and delete events
+
+**Thank You and Enjoy!**
--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tasks, setTasks] = useState([])
+    const [task, setTask] = useState('')
+
+
+useEffect(() => {
+    fetch('http://localhost:3000/tasks')
+    .then(res => res.json())
+    .then(data => setTasks(data))
+}, [])
+
+function addTask(e) {
+    e.preventDefault()
+    fetch('http://localhost:3000/tasks', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({task})
+    })
+    .then(res => res.json())
+    .then(data => {
+        setTasks([...tasks, data])
+        setTask('')
+    })
+    e.target.reset()
 }
 
-export default App;
+
+
+const displayTasks = tasks.map(task => {
+    return (
+        <div key={task.id}>
+            <h3>{task.task}</h3>
+            <button onClick={() => deleteTask(task.id)}>❌</button>
+        </div>
+    )
+
+})
+
+function deleteTask(id) {
+  fetch(`http://localhost:3000/tasks/${id}`, {
+    method: 'DELETE',
+  })
+  .then(res => res.json())
+  .then(() => {
+    setTasks(tasks.filter(task => task.id !== id)); 
+  });
+}
+
+
+  return (
+    <div className='tasklist'>
+        <h1>Your Todo List</h1>
+        <form onSubmit={addTask}>
+            <input type="text" placeholder="Add a task" value={task} onChange={(e) => setTask(e.target.value)} />
+        </form>
+        <ul>
+            {displayTasks}
+        </ul>
+
+    </div>
+  )
+}
+
+export default App
